5c0c30fd9f6caef13ad55c02414f9e231b9f3a43
mrstrike-droid <alexvershkov23@gmail.com>
2022-06-09 22:20:47 +0300

    first commit

---
+++ First look on massive.py
@@ -0,0 +1,10 @@
+print ('Please, enter your name')
+Name = str(input())
+print ('Please, enter your age')
+Age = int(input())
+print ('Please, enter your ocupations')
+Job = str(input())
+Lempl = [Name, Age, Job]
+print (Lempl)
+print("Press Enter to continue ...")
+input()
---
+++ Pr 1 (Asking name and age).py
@@ -0,0 +1,12 @@
+print ('Hello, Please enter your name:')
+Name = input()
+print ('Nice to meat you ' + Name)
+print ('What is your age:')
+Age = int(input())
+MyAge = (27)
+if Age > MyAge:
+    print ('You are older than me')
+else:
+    print ('You are younger than me')
+print("Press Enter to continue ...")
+input()
