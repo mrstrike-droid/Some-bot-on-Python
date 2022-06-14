f13cb9b3d28b19610465505016991549bb7dae9d
mrstrike-droid <alexvershkov23@gmail.com>
2022-06-14 22:59:11 +0300

    Chamge 1

--- Nasa API/Main Nasa API.py
+++ Nasa API/Main Nasa API.py
@@ -1,5 +1,15 @@
 import requests
-r = requests.get('https://api.nasa.gov/planetary/apod?api_key=Zex7CBAHQmbVfomUeIOyZXt9d8JccD4R50fNNhal')
-r1 = r.json()
-print (r1)
+def getimagine():
+    try:
+        date = input()
+        r = requests.get(f'https://api.nasa.gov/planetary/apod?api_key=Zex7CBAHQmbVfomUeIOyZXt9d8JccD4R50fNNhal&date={date}')
+        r1 = r.json()
+        print (r1)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print ("Looks like someone entered the wrong date format:",err)
+print('Please specifie the date in such way: YYYY-MM-DD')
+getimagine()
+input()
+
 
