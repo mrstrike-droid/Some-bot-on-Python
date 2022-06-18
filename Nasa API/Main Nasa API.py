cd58a0e374b1168cb8712c61d8df0146ab54cfa1
mrstrike-droid <alexvershkov23@gmail.com>
2022-06-18 18:00:54 +0300

    Major update

--- Nasa API/Main Nasa API.py
+++ Nasa API/Main Nasa API.py
@@ -1,23 +1,34 @@
 from re import T
 import requests
+import os
 def getimagine():
     while True:
         try:
             date = input()
             r = requests.get(f'https://api.nasa.gov/planetary/apod?api_key=Zex7CBAHQmbVfomUeIOyZXt9d8JccD4R50fNNhal&date={date}')
             parsed = r.json()
-            status =  r.status_code
-            print (status)
-            if status == 200:
+            if r.status_code == 200:
                 print (parsed)
-            elif status == 400:
+                url = parsed['hdurl']
+                img = requests.get (url) 
+                ext = img.headers["content-type"][6:]
+                print ('Chose your directory to save:(Click on the path bar of the Windows Explorer and tap Ctrl+C and then Ctrl+V)')
+                os.chdir(input())
+                os.makedirs(f'Nasa imagine/{date} Imagine of a day')
+                path = os.path.join(os.getcwd(), f'Nasa imagine/{date} Imagine of a day',f'img{date}.{ext}')
+                with open (path, 'wb') as f:
+                    for chunk in img:
+                        f.write(chunk) 
+                print ('Done!')
+                os.system("start" + path)           
+            elif r.status_code == 400:
                 print ('Wrong date type input: YYYY-MM-DD')
             else:
                 r.raise_for_status()    
         except requests.exceptions.HTTPError as err:
-            print (err)    
+            print (err)
 print('Please specifie the date in such way: YYYY-MM-DD. Actual on current or previous days')
 getimagine()
-input()
+
 
 
