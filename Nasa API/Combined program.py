a0911f031d71ed843a76c2a42418ab978b120d9f
mrstrike-droid <alexvershkov23@gmail.com>
2022-06-28 22:20:10 +0300

    Fix

--- Nasa API/Combined program.py
+++ Nasa API/Combined program.py
@@ -1,9 +1,9 @@
-import os
-os.system ('C:\Users\Aleksey\Desktop\Python\Automated borind staff\Nasa API\Nasa APOD.py')
-os.system ('C:\Users\Aleksey\Desktop\Python\Automated borind staff\Nasa API\Nasa Image Mars.py')
+import subprocess
+script1 = subprocess.call("NasaAPOD.py", shell=True)
+
 if __name__ == "__main__":
     chose_option = 0
     print('What would you like to save today: 1 - A picture of the day\n 2 - Pictures from Mars')
     chose_option = input()
     if chose_option == 1:
-        repeat_programm()
+        script1.repeat_programm()
