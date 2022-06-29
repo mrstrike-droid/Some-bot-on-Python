1fe7771e7248d0e6a035c5b3fbd805602f696509
mrstrike-droid <alexvershkov23@gmail.com>
2022-06-29 21:56:11 +0300

    Fix

--- Nasa API/Combined program.py
+++ Nasa API/Combined program.py
@@ -1,9 +1,20 @@
-import subprocess
-script1 = subprocess.call("NasaAPOD.py", shell=True)
-
-if __name__ == "__main__":
+import NasaAPOD
+import Nasa_Image_Mars
+def two_programm():
     chose_option = 0
-    print('What would you like to save today: 1 - A picture of the day\n 2 - Pictures from Mars')
+    print('What would you like to save today: 1 - A picture of the day\n2 - Pictures from Mars')
     chose_option = input()
     if chose_option == 1:
-        script1.repeat_programm()
+        NasaAPOD.NasaApod()
+    elif chose_option == 2:
+        Nasa_Image_Mars.repeatprogramm()
+if __name__ == "__main__":
+    restart = 1
+    while restart == 1:
+        try:
+            two_programm()
+            print('Whant to start again: 1 - Yes\n2 - No')
+            restart = input()
+        except ValueError:
+            print('Please enter 1 or 2')
+    
