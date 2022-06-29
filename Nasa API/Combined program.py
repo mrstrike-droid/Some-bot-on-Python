b9c133021315ca6c9c064693930a1dcd03ddf9c7
mrstrike-droid <alexvershkov23@gmail.com>
2022-06-29 22:43:47 +0300

    Done

--- Nasa API/Combined program.py
+++ Nasa API/Combined program.py
@@ -1,12 +1,17 @@
 import NasaAPOD
 import Nasa_Image_Mars
+import os
 def two_programm():
     chose_option = 0
     print('What would you like to save today: 1 - A picture of the day\n2 - Pictures from Mars')
-    chose_option = input()
+    chose_option = int(input())
     if chose_option == 1:
-        NasaAPOD.NasaApod()
+        print ('Chose your directory to save files:(Click on the path bar of the Windows Explorer and tap Ctrl+C and then Ctrl+V)')
+        os.chdir(input())
+        NasaAPOD.repeat_programm()
     elif chose_option == 2:
+        print ('Chose your directory to save files:(Click on the path bar of the Windows Explorer and tap Ctrl+C and then Ctrl+V)')
+        os.chdir(input())
         Nasa_Image_Mars.repeatprogramm()
 if __name__ == "__main__":
     restart = 1
