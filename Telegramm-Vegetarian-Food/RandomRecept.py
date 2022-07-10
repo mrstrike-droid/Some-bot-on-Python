3e49558d0259991ad88334f691ffd5391758f49a
mrstrike-droid <alexvershkov23@gmail.com>
2022-07-10 20:47:37 +0300

    Fix

--- Telegramm-Vegetarian-Food/RandomRecept.py
+++ Telegramm-Vegetarian-Food/RandomRecept.py
@@ -4,7 +4,6 @@ import random
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 def random_recipe():
-    global recipi, title, body, url, end
     rand = random.randrange(0,50)
     with open('C:\\Users\\Aleksey\\Desktop\\База данных вегеторианских рецептов\\Recepies.json','r', encoding='utf-8' ) as file:
         text = json.load(file)
@@ -19,23 +18,26 @@ def random_recipe():
     #pathimg = (f'C:\\Users\\Aleksey\\Desktop\\База данных вегеторианских рецептов\\Картинки\\{id_foto}.jpeg')
     #os.startfile(pathimg)    
 def search_recipe():
-    random_recipe()
-    while True:
-        string1 = recipi
+    with open('C:\\Users\\Aleksey\\Desktop\\База данных вегеторианских рецептов\\Recepies.json','r', encoding='utf-8' ) as file:
+        text = json.load(file)
         print('Введите ингредиенты: ')
         string2=str(input())
-        unwanted_characters = ".,!?"
-        string1_words = set(string1.split())
-        string2_words = set(string2.split())
-        string1_words = {word.strip(unwanted_characters) for word in string1_words}
-        string2_words = {word.strip(unwanted_characters) for word in string2_words}
-        string2_words = {word.strip(unwanted_characters).lower() for word in string2_words}
-        common_words = string1_words & string2_words
-        if len(common_words) >= 3:
-            print(title+end+body+end+'Список ингридиентов: '+recipi+end+'Ссылка на рецепт: '+url)
-            break
-        else:
-            random_recipe()
+        for i in text:
+            recipi1 = i[3]
+            string1 = recipi1
+            unwanted_characters = ".,!?"
+            string1_words = set(string1.split())
+            string2_words = set(string2.split())
+            string1_words = {word.strip(unwanted_characters) for word in string1_words}
+            string2_words = {word.strip(unwanted_characters) for word in string2_words}
+            string2_words = {word.strip(unwanted_characters).lower() for word in string2_words}
+            common_words = string1_words & string2_words
+            if len(common_words) >= 3:
+                print(recipi1)
+                #print(title+end+body+end+'Список ингридиентов: '+recipi+end+'Ссылка на рецепт: '+url)
+                break
+            else:
+                print('Ищем...')
 search_recipe()
 
 
