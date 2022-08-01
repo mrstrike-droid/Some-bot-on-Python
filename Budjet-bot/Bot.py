9d7fdeb2861d226dfd93dd48c9c60ca2dfb97e75
mrstrike-droid <alexvershkov23@gmail.com>
2022-08-01 23:12:41 +0300

    Fix

--- Budjet-bot/Bot.py
+++ Budjet-bot/Bot.py
@@ -2,12 +2,15 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import numpy as np
+def range_char(start, stop):
+    return (chr(n) for n in range(ord(start), ord(stop) + 1))
 scope = ['https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\Aleksey\\Desktop\\Python\\Automated borind staff\\Budjet-bot\\client_secret.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open("Бюджет")
 list_of_hashes = sheet.get_worksheet(1)
-list_of_hashes = list_of_hashes.get_all_records()
-print(list_of_hashes)
+for character in range_char("c", "z"):
+    column_index = character.upper
+list_of_hashes.update(f'{column_index}1', '01.08.2022')
 input()
 
