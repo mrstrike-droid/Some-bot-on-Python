a31f43ce6229ee02e91e87e98ee7213c7c064787
mrstrike-droid <alexvershkov23@gmail.com>
2022-08-02 21:33:27 +0300

    Update

--- Budjet-bot/Bot.py
+++ Budjet-bot/Bot.py
@@ -2,16 +2,35 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import numpy as np
-def range_char(start, stop):
-    return (chr(n) for n in range(ord(start), ord(stop) + 1))
+from datetime import date
 scope = ['https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\Aleksey\\Desktop\\Python\\Automated borind staff\\Budjet-bot\\client_secret.json', scope)
 client = gspread.authorize(creds)
+day_today = str(date.today())
 sheet = client.open("Бюджет")
 list_of_hashes = sheet.get_worksheet(1)
-for character in range_char("c", "y"):
-    column_index = character
-    print(column_index)
-    list_of_hashes.update(f'{column_index}1', '01.08.2022')
-input()
+def update_info_in_next_day():
+    global i
+    for i in range(3,24):
+        cell_info = list_of_hashes.cell(1, f'{i}').value
+        if cell_info == None:
+            list_of_hashes.update_cell( 1, f'{i}', f'{day_today}')
+            break
+def insert_info_in_cell_and_get_residue():
+    print('Что мы потратили сегодня на продукты:')
+    product = int(input())
+    list_of_hashes.update_cell( 2, f'{i}', f'{product}')
+    cell_info = list_of_hashes.cell(2, 2).value
+    print('На продукты осталось: ', cell_info)
+update_info_in_next_day()
+insert_info_in_cell_and_get_residue()
+def update_info_again():
+    global i
+    for i in range(3,24):
+        cell_info = list_of_hashes.cell(1, f'{i}').value
+        if cell_info == day_today:
+            list_of_hashes.update_cell( 2, f'{i}', 'Done')
+            break
+#update_info_again()
+#insert_info_in_cell_and_get_residue()
 
