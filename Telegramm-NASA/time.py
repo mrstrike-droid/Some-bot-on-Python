598b2e5a3a3c01d370d725a29589d1656ed9559f
mrstrike-droid <alexvershkov23@gmail.com>
2023-02-01 20:42:44 +0200

    Update

--- Telegramm-NASA/time.py
+++ Telegramm-NASA/time.py
@@ -1,22 +1,14 @@
 from datetime import datetime
 from datetime import timedelta
 import datetime
-
-def change_date(): 
-    global z
-    z = datetime.datetime(year=2023,month=1,day=31,hour=20,minute=28)
-    z = z+timedelta(days = 1)
-    print(z)
 def compare_time(): 
     z = datetime.datetime(year=2023,month=1,day=31,hour=20,minute=50).strftime("%Y-%m-%d %H:%M")
     while True:        
         x = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        print(x)
-        print(z)
         if x == z:
             z = datetime.datetime.strptime(z, '%Y-%m-%d %H:%M')
             z = z+timedelta(days = 1)
             z= datetime.datetime.strftime(z, "%Y-%m-%d %H:%M")
             print(z)
 compare_time()
-#change_date()
+
