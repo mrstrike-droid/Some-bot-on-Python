a3805fb357bd4a7d294ed155b5825fbff0a2a8a4
mrstrike-droid <alexvershkov23@gmail.com>
2023-04-24 22:37:41 +0300

    Commit

--- Application for new series/Parser.py
+++ Application for new series/Parser.py
@@ -4,10 +4,39 @@ from time import sleep
 import os
 import json
 data = []
-headers = {'user-agent': 'my-app/0.0.1'}
-url = 'https://rezka.ag/+'
+data2=[]
+data3=[]
+count=0
+count2=0
+headers = {'user-agent': 'my-app/0.0.2'}
+url = 'https://rezka.ag'
 r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.text, 'lxml')
-soup = soup.find_all('div', class_='b-content__inline_sidebar')
-print(soup)
+Text_and_urls = soup.find('div', class_='b-content__inline_sidebar').find_all('div', class_='b-seriesupdate__block')
+for i in Text_and_urls:
+    count+=1
+    data.append(count)
+    tech_var= i.find_all('li', class_='b-seriesupdate__block_list_item')
+    for x in tech_var:                
+        data.append(x.find('div', class_='b-seriesupdate__block_list_item_inner').find('div', class_='cell cell-1').find('a', class_='b-seriesupdate__block_list_link').text)
+date = soup.find('div', class_='b-content__inline_sidebar').find_all('div', class_='b-seriesupdate__block')
+for z in date:
+    tech_var2=z.find_all('div', class_='b-seriesupdate__block_date')
+    tech_var3=z.find_all('div', class_='b-seriesupdate__block_date collapsible')
+    for y in tech_var2:
+        y=y.find('small')
+        y=str(y)
+        y=y.replace('<small>(','')
+        y=y.replace(')</small>','')
+        data2.append(y)
+    for y in tech_var3:
+        y=y.text
+        y=str(y)
+        y=y.replace(' развернуть','')
+        data2.append(y) 
+for k in data2:
+    if k != 'None':
+        data3.append(k)
+print(data)
+
 
