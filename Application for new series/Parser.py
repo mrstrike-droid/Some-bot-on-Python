ddf0f429c6a80ab4a5827bb211bc0a830634b18d
mrstrike-droid <alexvershkov23@gmail.com>
2023-04-25 22:01:39 +0300

    Commit

--- Application for new series/Parser.py
+++ Application for new series/Parser.py
@@ -1,26 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from itertools import chain
 import os
 import json
-data = []
-data2=[]
-data3=[]
-count=0
-count2=0
+list_of_serial_names=[]
+tech_var_list=[]
+list_of_serial_urls=[]
+list_of_lists=[]
+dict_with_set_of_date_and_serials = {}
 headers = {'user-agent': 'my-app/0.0.2'}
 url = 'https://rezka.ag'
 r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.text, 'lxml')
-Text_and_urls = soup.find('div', class_='b-content__inline_sidebar').find_all('div', class_='b-seriesupdate__block')
-for i in Text_and_urls:
-    count+=1
-    data.append(count)
-    tech_var= i.find_all('li', class_='b-seriesupdate__block_list_item')
-    for x in tech_var:                
-        data.append(x.find('div', class_='b-seriesupdate__block_list_item_inner').find('div', class_='cell cell-1').find('a', class_='b-seriesupdate__block_list_link').text)
-date = soup.find('div', class_='b-content__inline_sidebar').find_all('div', class_='b-seriesupdate__block')
-for z in date:
+text_and_urls = soup.find('div', class_='b-content__inline_sidebar').find_all('div', class_='b-seriesupdate__block')
+for i in text_and_urls:
+    tech_var= i.find_all('li', class_='b-seriesupdate__block_list_item') 
+    for x in tech_var:
+        list_of_serial_names=(x.find('div', class_='b-seriesupdate__block_list_item_inner').find('div', class_='cell cell-1').find('a', class_='b-seriesupdate__block_list_link').text)
+        list_of_serial_urls=(x.find('div', class_='b-seriesupdate__block_list_item_inner').find('div', class_='cell cell-1').find('a', class_='b-seriesupdate__block_list_link').get('href'))
+        list_of_lists=([list_of_serial_names,list_of_serial_urls])
+date_var = soup.find('div', class_='b-content__inline_sidebar').find_all('div', class_='b-seriesupdate__block')
+for z in date_var:
     tech_var2=z.find_all('div', class_='b-seriesupdate__block_date')
     tech_var3=z.find_all('div', class_='b-seriesupdate__block_date collapsible')
     for y in tech_var2:
@@ -28,15 +29,18 @@ for z in date:
         y=str(y)
         y=y.replace('<small>(','')
         y=y.replace(')</small>','')
-        data2.append(y)
+        tech_var_list.append(y)
     for y in tech_var3:
         y=y.text
         y=str(y)
         y=y.replace(' развернуть','')
-        data2.append(y) 
-for k in data2:
-    if k != 'None':
-        data3.append(k)
-print(data)
+        tech_var_list.append(y) 
+date_list = [k for k in tech_var_list if k != 'None']
+for key in date_list:
+    for value in list_of_lists:
+        dict_with_set_of_date_and_serials[key] = value
+        list_of_lists.remove(value)
+        break
+print(dict_with_set_of_date_and_serials)
 
 
